5ed1683739978ec4c5adff53c42db17d8c3608a4
Denise Laurence Miguel <163559466+Denise030804@users.noreply.github.com>
2024-07-23 19:11:58 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def main(page: ft.Page):
 
     page.on_route_change = route_change
 
-    # Initialize database
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users
